3c3c0d6e286775beb06a6dd048d6df5ad5ca3a27
Ankur Raj <64456355+ancoreraj@users.noreply.github.com>
2022-01-09 13:40:29 +0530

    V1 complete

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const { ApolloServer } = require('apollo-server-express')
 const app = express();
 const connectDB = require('./config/db')
-
+const { typeDefs } = require('./graphQL/typeDefs')
+const { resolvers } = require('./graphQL/resolvers')
 
 dotenv.config({ path: './config/.env' })
 
-connectDB()
+let apolloServer = null;
+async function startServer() {
+    apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+    connectDB()
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+}
+startServer();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
+
